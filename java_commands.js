d6684973129e6b16d0fcdf98f2bc506dc67fcc09
Stuart Hiller <hiller.stuart@gmail.com>
2015-07-12 17:57:32 -0700

    updated game

--- java_commands.js
+++ java_commands.js
@@ -1,11 +1,11 @@
-function Game(gameName, question, answers, messages, pictures) {
+function Game(gameName, question, answers, messages) {
   this.gameName = gameName;
   this.question = question;
   this.answers = answers;
   this.response = "It doesn't Matter";
   this.messages = messages;
   this.run = function() {
-    var elMessage = document.getElementById("demo")
+    var elMessage = document.getElementById("response")
   var guess = prompt(this.question);
     for (var i = 0; i <= this.answers.length; i++) {
       if (guess.toUpperCase() === this.answers[i]) {
@@ -14,9 +14,13 @@ function Game(gameName, question, answers, messages, pictures) {
       }
       }
       elMessage.textContent = this.response;
-      document.getElementById("demo_2").innerHTML = guess;
+      document.getElementById("what they wrote").innerHTML = guess;
     }
   }
 var movieGuess = new Game("Movie Guesses", "What is your favorite movie.", ["MY OWN PRIVATE IDAHO", "BLADE RUNNER", "BLOOD IN BLOOD OUT"], ["Rad", "Thats a pretty cliche answer", "One of those best worst movie situations"]);
 
+//function myFunction() {
+//    var x = document.getElementById("myInput").value;
+//    document.getElementById("demo").innerHTML = "You wrote: " + x;
+//}
 movieGuess.run();
